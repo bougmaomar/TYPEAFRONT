7bf90ade382e3bc1794be203b0e044ecc33c2bd2
unknown <omarbgm4@gmail.com>
2022-05-17 07:01:40 +0100

    fixed error

--- src/app/demandeur/postuler/login/login.component.ts
+++ src/app/demandeur/postuler/login/login.component.ts
@@ -1,38 +1,23 @@
-<<<<<<< HEAD
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/controller/model/user.model';
 import { AllusersService } from 'src/app/controller/service/allusers.service';
 import { VarService } from 'src/app/controller/service/var.service';
-=======
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
-import {User} from 'src/app/controller/model/user.model';
-import {AllusersService} from 'src/app/controller/service/allusers.service';
->>>>>>> e2ca47535abf211f28e4c331f849dd356920aac3
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
-  @Input() isLogged: boolean = false;
+export class LoginComponent {
+  isLogged: boolean = false;
   user: User = new User();
-<<<<<<< HEAD
+
   constructor(
     private allusersService: AllusersService,
     private router: Router,
     private varService: VarService
   ) {}
-=======
-
-  constructor(private allusersService: AllusersService,
-              private router: Router
-  ) {
-  }
->>>>>>> e2ca47535abf211f28e4c331f849dd356920aac3
-
   ngOnInit(): void {}
 
   onSubmit() {
@@ -45,20 +30,4 @@ export class LoginComponent implements OnInit {
       }
     });
   }
-<<<<<<< HEAD
-=======
-
-  addUser() {
-    this.allusersService.loginUser(this.user).subscribe(data => {
-      console.log(data);
-      console.log("here")
-    })
-  }
-
-  onSubmit() {
-    console.log(this.user);
-    this.addUser();
-  }
-
->>>>>>> e2ca47535abf211f28e4c331f849dd356920aac3
 }
