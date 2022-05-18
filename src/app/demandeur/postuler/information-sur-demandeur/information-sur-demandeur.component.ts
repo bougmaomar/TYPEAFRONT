b9aec5935215c6c736a85aec341d537fb9d3818b
unknown <omarbgm4@gmail.com>
2022-05-18 22:57:13 +0100

    donnepro fixed

--- src/app/demandeur/postuler/information-sur-demandeur/information-sur-demandeur.component.ts
+++ src/app/demandeur/postuler/information-sur-demandeur/information-sur-demandeur.component.ts
@@ -3,31 +3,40 @@ import { Router } from '@angular/router';
 import { DonneePro } from 'src/app/controller/model/donnee-pro.model';
 import { AllusersService } from 'src/app/controller/service/allusers.service';
 import { UserService } from 'src/app/controller/service/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-information-sur-demandeur',
   templateUrl: './information-sur-demandeur.component.html',
-  styleUrls: ['./information-sur-demandeur.component.css']
+  styleUrls: ['./information-sur-demandeur.component.css'],
 })
 export class InformationSurDemandeurComponent implements OnInit {
-
   donne: DonneePro = new DonneePro();
-  constructor(private userService: UserService,
-              private router: Router
-  ) { }
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    this.userService.saveDonnesPro(this.donne).subscribe((data: any) => {
+      if (data == -1) {
+        Swal.fire(
+          'Mise a jour données',
+          'Vos données professionnels ont ete mise a jour',
+          'success'
+        );
+      } else if (data == 1) {
+        Swal.fire(
+          'Ajout données',
+          'Vos données professionnels ont ete ajouter',
+          'success'
+        );
+      } else {
+        Swal.fire(
+          'Ajout données',
+          'Veuillez verfiez que un ou plusieur champs sont pas vide',
+          'error'
+        );
+      }
+    });
   }
-
-  addUser(){
-    this.userService.saveDonnesPro(this.donne).subscribe(data =>{
-      console.log(data);
-    })
-  }
-
-  onSubmit(){
-    console.log(this.donne);
-    this.addUser();
-  }
-
 }
